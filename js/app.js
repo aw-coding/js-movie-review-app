d3d753a39882a6775161c07ec34dbffc0097657f
Andrew Wood <andrewwood90@hotmail.com>
2021-01-10 17:49:24 +0000

    mvp complete

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const review = document.querySelector('#add-item-form');
     review.addEventListener('submit', getReview);
 
+    const deleteAll = document.querySelector('#delete-button');
+    deleteAll.addEventListener('click', deleteAllReviews);
+
 
 
 })
@@ -11,9 +14,14 @@ document.addEventListener("DOMContentLoaded", () => {
 const getReview = (event) => {
     event.preventDefault();
     const newListItem = document.createElement('li');
-    newListItem.textContent = `${event.target.title.value}`;
+    newListItem.textContent = `${event.target.title.value} ${event.target.director.value} ${event.target.review.value}`;
     newListItem.classList.add('movie-reviews');
     const list = document.querySelector('ul');
     list.appendChild(newListItem);
     document.getElementById('add-item-form').reset();
+}
+
+const deleteAllReviews = () => {
+    const allLiElements = document.querySelector('#current-reviews');
+    allLiElements.innerHTML = ' ';
 }
